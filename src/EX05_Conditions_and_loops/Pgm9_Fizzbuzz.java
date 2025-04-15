8a5889dff99e06f28137c18380492fab962ca16a
Suryaram-qa <suriindhumaha@gmail.com>
2025-04-15 23:12:14 +0530

    April 15 commit

---
+++ src/EX05_Conditions_and_loops/Pgm9_Fizzbuzz.java
@@ -0,0 +1,19 @@
+package EX05_Conditions_and_loops;
+
+public class Pgm9_Fizzbuzz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else
+                System.out.println(i);
+        }
+
+    }
+}
